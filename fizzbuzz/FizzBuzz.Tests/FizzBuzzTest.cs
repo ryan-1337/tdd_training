18a95e4a3c76d1aa9776b5e3fdddc34ee2f363cf
ryan-1337 <ryanaouiche@gmail.com>
2025-05-25 01:24:31 +0200

    starting fizzbuzz kata

---
+++ fizzbuzz/FizzBuzz.Tests/FizzBuzzTest.cs
@@ -0,0 +1,38 @@
+﻿namespace FizzBuzz.Tests;
+
+public class FizzBuzzTest
+{
+    private readonly FizzBuzz FizzBuzz = new FizzBuzz();
+    
+    [Theory]
+    [InlineData(1)]
+    [InlineData(2)]
+    [InlineData(4)]
+    [InlineData(7)]
+    public void Should_ReturnNumberInStringWhenGivenRandomNumber(int number)
+    {
+        // given a random number
+             
+        // when the int equal random number
+        String response = FizzBuzz.Generate(number);
+             
+        // then should return "X"
+        Assert.Equal(number.ToString(), response);
+    }
+
+    [Theory] 
+    [InlineData(3)]
+    [InlineData(6)]
+    [InlineData(9)]
+    [InlineData(12)]
+    public void Should_ReturnFizzWhenGivenMultipleOfThree(int number)
+    {
+        // given a multiple of three
+        
+        // when the int equal multiple of three
+        String response = FizzBuzz.Generate(number);
+
+        // then should return "Fizz"
+        Assert.Equal("Fizz", response);
+    }
+}
---
+++ fizzbuzz/FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,13 @@
+﻿namespace FizzBuzz;
+
+public class FizzBuzz
+{
+    public string Generate(int number)
+    {
+        if (number % 3 == 0)
+        {
+            return "Fizz";
+        }
+        return number.ToString();
+    }
+}
