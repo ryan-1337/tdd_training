d5485ecf113947f7643cc0a73671ee790149acaf
ryan-1337 <ryanaouiche@gmail.com>
2025-05-24 14:48:37 +0200

    add stack kata

---
+++ Stack/unit-testing-using-dotnet-test/StackService.Tests/StackTest.cs
@@ -0,0 +1,81 @@
+﻿using Xunit.Sdk;
+
+namespace Stack.Tests;
+
+public class StackTest
+{
+  private readonly StackService stack = new StackService();
+
+  [Fact]
+  public void StackEmpty_WhenStackCreate()
+  {
+    // given an empty stack
+    
+    // when stack created
+    
+    // then stack should be empty
+    Assert.True(stack.IsEmpty());
+  }
+  
+  [Fact]
+  public void AfterOnePush_StackIsNotEmpty()
+  {
+    // given an empty stack
+    
+    // when adding something
+    stack.Push(1);
+    
+    // then stack is not empty
+    Assert.False(stack.IsEmpty());
+  }
+
+  [Fact]
+  public void AfterPop_WhenEmptyStackThrowUnderflow()
+  {
+    // given an empty stack
+    
+    // when trying to remove something
+    
+    // then there should be an Error
+    Assert.Throws<InvalidOperationException>(() => stack.Pop());
+  }
+
+  [Fact]
+  public void AfterOnePushAndOnePop_StackWillBeEmpty()
+  {
+    // given an empty stack
+    
+    // when adding something and removing it 
+    stack.Push(1);
+    stack.Pop();
+    
+    // then stack should be empty
+    Assert.True(stack.IsEmpty());
+  }
+  
+  [Fact]
+  public void AfterTwoPushAndOnePop_StackWillBeNotEmpty()
+  {
+    // given an empty stack
+    
+    // when adding two elements on stack and remove one
+    stack.Push(1);
+    stack.Push(1);
+    stack.Pop();
+    
+    // then stack should not be empty
+    Assert.False(stack.IsEmpty());
+  }
+
+  [Fact]
+  public void AfterOnePushAndOnePop_ReturnThePopValue()
+  {
+    // given an empty stack
+    
+    // when adding one element to stack
+    stack.Push(1);
+    
+    // then should return the element value
+    Assert.Equal(1, stack.Pop());
+  }
+}
---
+++ Stack/unit-testing-using-dotnet-test/StackService/StackService.cs
@@ -0,0 +1,27 @@
+﻿namespace Stack;
+public class StackService
+{
+  private int size = 0;
+  private int element = 0;
+
+  public bool IsEmpty()
+  {
+    return size == 0;
+  }
+  
+  public void Push(int element)
+  {
+    this.element = element;
+    size++;
+  }
+
+  public int Pop()
+  {
+    if (IsEmpty())
+    {
+      throw new InvalidOperationException("Underflow");
+    }
+    size--;
+    return element;
+  }
+}
