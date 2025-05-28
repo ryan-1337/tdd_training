fc4ab8bda0e354022ea373be8b2ab40d99d7345f
ryan-1337 <ryanaouiche@gmail.com>
2025-05-28 02:20:53 +0200

    [fizzbuzz] - wip

--- fizzbuzz/FizzBuzz.Tests/FizzBuzzTest.cs
+++ fizzbuzz/FizzBuzz.Tests/FizzBuzzTest.cs
@@ -25,14 +25,98 @@ public class FizzBuzzTest
     [InlineData(6)]
     [InlineData(9)]
     [InlineData(12)]
-    public void Should_ReturnFizzWhenGivenMultipleOfThree(int number)
+    public void Should_ReturnFizzWhenGivenMultipleOfThree(int multipleOf3)
     {
         // given a multiple of three
         
         // when the int equal multiple of three
-        String response = FizzBuzz.Generate(number);
+        String response = FizzBuzz.Generate(multipleOf3);
 
         // then should return "Fizz"
         Assert.Equal("Fizz", response);
     }
+
+    [Theory]
+    [InlineData(5)]
+    [InlineData(10)]
+    [InlineData(20)]
+    public void Should_ReturnBuzzWhenMultipleOfFive(int multipleOf5)
+    {
+        // given a multiple of five
+        
+        // when the int equal multiple of five
+        String response = FizzBuzz.Generate(multipleOf5);
+        
+        // then should return "Buzz"
+        Assert.Equal("Buzz", response);
+    }
+    
+    [Theory]
+    [InlineData(15)]
+    [InlineData(30)]
+    [InlineData(45)]
+    [InlineData(60)]
+    public void Should_ReturnFizzBuzzWhenGivenMultipleOfFifteen(int multipleOf15)
+    {
+        // given a multiple of fifteen
+            
+        // when the int equal fifteen
+        String response = FizzBuzz.Generate(multipleOf15);
+            
+        // then should return "FizzBuzz";
+        Assert.Equal("FizzBuzz", response);
+    }
+
+    [Fact]
+    public void Should_ReturnOneAndTwo()
+    {
+        // given int two
+        var oneTwo = 2;
+
+        // when the int equal 2
+        String response = FizzBuzz.GenerateLoop(2);
+
+        // then should return "12"
+        Assert.Equal("12", response);
+    }
+    
+    [Fact]
+    public void Should_ReturnOneAndTwoAndFizz()
+    {
+        // given int three
+        var three = 3;
+ 
+        // when the int equal 3
+        String response = FizzBuzz.GenerateLoop(three);
+ 
+        // then should return "12Fizz"
+        Assert.Equal("12Fizz", response);
+    }   
+     
+    [Fact]
+    public void Should_ReturnOneTwoFizzFour()
+    {
+        // given int 4
+        var four = 4;
+      
+        // when the int 4
+        String response = FizzBuzz.GenerateLoop(four);
+      
+        // then should return "12Fizz4"
+        Assert.Equal("12Fizz4", response);
+    }
+    
+    [Fact]
+    public void Should_ReturnOneTwoFizzFourBuzz()
+    {
+        // given int 5
+        var five = 5;
+       
+        // when the int 4
+        String response = FizzBuzz.GenerateLoop(five);
+       
+        // then should return "12Fizz4"
+        Assert.Equal("12Fizz4Buzz", response);
+    }   
+    
 }
--- fizzbuzz/FizzBuzz/FizzBuzz.cs
+++ fizzbuzz/FizzBuzz/FizzBuzz.cs
@@ -4,10 +4,32 @@ public class FizzBuzz
 {
     public string Generate(int number)
     {
+        if (number % 15 == 0)
+        {
+            return "FizzBuzz";
+        }
+        
         if (number % 3 == 0)
         {
             return "Fizz";
         }
+        
+        if (number % 5 == 0)
+        {
+            return "Buzz";
+        }
+        
         return number.ToString();
+    }
+
+    public string GenerateLoop(int number)
+    {
+        var response = "";
+        for (var i = 1; i <= number; i++)
+        {
+            response += Generate(i);
+        }
+
+        return response;
     }
 }
